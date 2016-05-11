e0dd477c114b96c599ff2b791de7c3be00b5d0b2
Miarviolin <miarmamdouh94@gmail.com>
2016-05-11 20:51:34 +0200

    post

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511111800) do
+ActiveRecord::Schema.define(version: 20160511180751) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "body",       limit: 65535, null: false
     t.string   "name",       limit: 255,   null: false
     t.string   "website",    limit: 255
     t.string   "email",      limit: 255,   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
